ab45eeb63ed4fd16fb0a799d1f9628a0ee009d09
emapellizza <emapellizza@gmail.com>
2023-10-16 17:45:11 -0300

    M01 hasta M03 completo M04 hasta funcion saludo completos

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,9 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+      if(x > y){
+         return(x)
+      }else return (y)
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +15,9 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   if(edad >= 18){
+      return("Allowed")
+   }else return ("Not allowed")
 }
 
 function conection(status) {
@@ -21,6 +27,11 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   if(status === 1){
+      return ("Online");
+   } else if( status === 2){
+      return ("Away")
+   }else return("Offline")
 }
 
 function saludo(idioma) {
@@ -30,6 +41,23 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
+   switch(idioma){
+      case 'aleman':
+         return ('Guten Tag!')
+         break;
+
+      case 'mandarin':
+         return ('Ni Hao!');
+         break;
+
+      case 'ingles':
+         return ('Hello!');
+         break;
+
+      default:
+         return ('Hola!');
+
+   }
 }
 
 function colors(color) {
