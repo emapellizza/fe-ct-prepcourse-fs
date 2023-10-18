9c4cc96b5ae03a89db019e6c38f6199741f2b3e8
emapellizza <emapellizza@gmail.com>
2023-10-18 16:22:02 -0300

    estoy en funcion de palabra a frase m05

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -3,34 +3,48 @@
 function devolverPrimerElemento(array) {
    // Retornar el primer elemento del arreglo recibido por parámetro.
    // Tu código:
+   return (array[0]);
 }
 
 function devolverUltimoElemento(array) {
    // Retornar el último elemento del arreglo recibido por parámetro.
    // Tu código:
+
+   return(array[array.length - 1])
 }
 
 function obtenerLargoDelArray(array) {
    // Retornar la longitud del arreglo recibido por parámetro.
    // Tu código:
+   return(array.length)
 }
 
 function incrementarPorUno(array) {
    // El arreglo recibido por parámetro contiene números.
    // Retornar un arreglo con los elementos incrementados en +1.
    // Tu código:
+   var miArray=[];
+
+   for(let i = 0; array.length > i; i++){
+    miArray[i]= array[i]+1
+   }
+   return(miArray)
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.push(elemento);
+   return(array)
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
    // Agrega el "elemento" al comienzo del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.unshift(elemento);
+   return(array)
 }
 
 function dePalabrasAFrase(palabras) {
@@ -39,6 +53,17 @@ function dePalabrasAFrase(palabras) {
    // con un espacio entre cada palabra.
    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'.
    // Tu código:
+   let miString=[]
+
+   for(let i = 0; palabras.length > i; i++){
+
+    miString[i] = palabras[i]+" "
+
+   }
+
+miString = miString.toString();
+return(miString)
+
 }
 
 function arrayContiene(array, elemento) {
---
+++ M05 JavaScript Arrays/mitest.js
@@ -0,0 +1,24 @@
+
+// El argumento "palabras" es un arreglo de strings.
+   // Retornar un string donde todas las palabras estén concatenadas
+   // con un espacio entre cada palabra.
+   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'.
+   // Tu código:
+
+let array=["hola","buenos","dias"]
+
+
+let miString
+
+
+for(let i = 0; array.length > i; i++){
+
+   //console.log(array[i])
+   miString = " " + array[i]
+}
+
+//miString=array[0]+" "+array[1]+" "+array[2]
+
+//miString = miString.toString();
+
+console.log(miString)
