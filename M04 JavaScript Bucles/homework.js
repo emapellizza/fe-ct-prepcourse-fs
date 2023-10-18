ad1f40a67eb416df604d2d924af029b608b01c64
emapellizza <emapellizza@gmail.com>
2023-10-18 12:24:39 -0300

    mo4 resuelto completo

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -173,6 +173,23 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   if (num <= 1) {
+      return false;
+    }
+    if (num <= 3) {
+      return true;
+    }
+    if (num % 2 === 0 || num % 3 === 0) {
+      return false;
+    }
+    
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) {
+        return false;
+      }
+   }
+    return true;
+   
 
 }
 
